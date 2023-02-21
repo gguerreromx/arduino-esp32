124332833e96f8ea38439e3fca8a5dc97e06a18e
Gustavo Guerrero <13749076+gguerreromx@users.noreply.github.com>
2023-02-21 14:37:58 -0600

    Add pinout for Heltec Wireless Stick Lite V3
    Added the pinout of the v3 of heltec wireless stick lite v3. 
    
    Based on documentation from: 
    
    https://heltec.org/project/wireless-stick-lite-v2/ (the  v2 seems that they did not  bother to create a new page)
    
    Direct PDF download:
    https://resource.heltec.cn/download/Wireless_Stick_Lite_V3/HTIT-WSL_V3(Rev1.1).pdf
    
    Also the esp32 s3 technical manual was used, since  the above pdf does not include I2C and

---
+++ variants/heltec_wireless_stick_lite_v3/pins_arduino.h
@@ -0,0 +1,62 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include <stdint.h>
+
+#define Wireless_Stick_Lite true
+#define DISPLAY_HEIGHT 0
+#define DISPLAY_WIDTH  0
+
+#define EXTERNAL_NUM_INTERRUPTS 16
+#define NUM_DIGITAL_PINS        40
+#define NUM_ANALOG_INPUTS       14
+
+#define analogInputToDigitalPin(p)  (((p)<20)?(analogChannelToDigitalPin(p)):-1)
+#define digitalPinToInterrupt(p)    (((p)<40)?(p):-1)
+#define digitalPinHasPWM(p)         (p < 34)
+
+static const uint8_t LED_BUILTIN = 35;
+#define BUILTIN_LED  LED_BUILTIN // backward compatibility
+#define LED_BUILTIN LED_BUILTIN
+
+static const uint8_t KEY_BUILTIN = 0;
+
+static const uint8_t TX = 44;
+static const uint8_t RX = 43;
+
+static const uint8_t SDA = 2;
+static const uint8_t SCL = 3;
+
+static const uint8_t SS    = 34;
+static const uint8_t MOSI  = 35;
+static const uint8_t SCK   = 36;
+static const uint8_t MISO  = 37;
+
+static const uint8_t A0 = 1;
+static const uint8_t A3 = 2;
+static const uint8_t A4 = 3;
+static const uint8_t A5 = 4;
+static const uint8_t A6 = 5;
+static const uint8_t A7 = 6;
+static const uint8_t A10 = 12;
+static const uint8_t A11 = 14;
+static const uint8_t A12 = 15;
+static const uint8_t A13 = 16;
+static const uint8_t A14 = 17;
+static const uint8_t A15 = 18;
+static const uint8_t A16 = 19;
+static const uint8_t A17 = 20;
+
+static const uint8_t T0 = 1;
+static const uint8_t T1 = 2;
+static const uint8_t T2 = 3;
+static const uint8_t T3 = 4;
+static const uint8_t T4 = 5;
+static const uint8_t T5 = 6;
+static const uint8_t T6 = 7;
+static const uint8_t T7 = 14;
+
+static const uint8_t Vext = 36;
+static const uint8_t LED  = 35;
+
+#endif /* Pins_Arduino_h */
